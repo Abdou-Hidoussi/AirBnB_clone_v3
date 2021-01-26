b60a0b66131b317a6f693b834821686e4efd2120
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-01-26 22:17:43 +0100

    task five

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,17 @@ from models import storage
 def all_states():
     """ Task 4 """
     return jsonify(status="OK")
+
+
+@app_views.route('/stats')
+def stat_count():
+    """ Task 5 """
+    count_stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(count_stats)
