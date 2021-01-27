878cfc0709c2e65dff08ae1da3c0a7cdcc9c5f82
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-01-27 22:42:37 +0100

    update get/put/delete/getbystate func

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,43 +3,44 @@
 from flask import Flask, Blueprint, jsonify, request, abort, make_response
 from api.v1.views import app_views
 from models import storage
+from models.city import City
+from models.state import State
+
+
+@app_views.route('/states/<string:state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_cities(state_id):
+    """get city information for all cities in a specified state"""
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    cities = []
+    for city in state.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
 
 
 @app_views.route('/cities/<city_id>', strict_slashes=False)
 def retrive_City(city_id):
     """ Task 7 """
-    city = storage.get("City", city_id)
-    if city is None:
-        abort(404)
-    return jsonify(city.to_dict())
-
-
-@app_views.route('/cities/<city_id>', methods=['DELETE'])
-def delete_City(city_id):
-    """ Task 7 """
-    to_del = storage.get("City", city_id)
-    if to_del:
-        storage.delete(to_del)
-        storage.save()
-        return jsonify({})
+    for ct in storage.all("City").values():
+        if ct.id == city_id:
+            return jsonify(ct.to_dict())
     abort(404)
+    
 
-
-@app_views.route('/cities/<city_id>', methods=['PUT'])
-def update_City(city_id=None):
-    """ Task 7 """
-    to_update = storage.get("City", city_id)
-    req = request.get_json()
-
-    if to_update is None:
+def post_city(state_id):
+    """Task 7 create city in state"""
+    state = storage.get("State", state_id)
+    if state is None:
         abort(404)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if "name" not in request.get_json():
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    kwargs = request.get_json()
+    kwargs['state_id'] = state_id
+    city = City(**kwargs)
+    city.save()
+    return make_response(jsonify(city.to_dict()), 201)
 
-    if req is None:
-        abort(400)
-
-    for key, value in req.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(to_update, key, value)
-
-    storage.save()
-    return jsonify(to_update.to_dict())
