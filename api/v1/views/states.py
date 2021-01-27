b5b57fe93ba0c17448ee927bf6b80b0e34e5147b
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-01-27 19:33:46 +0000

    initial

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,6 +3,7 @@
 from flask import Flask, Blueprint, jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
+from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False)
