173e22fb7fbc80bd46dbe8e5b6e7f22716141f69
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-01-28 06:01:07 +0000

    restructure

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from models import storage
 from models.city import City
 from models.place import Place
+from models.user import User
 
 
 @app_views.route('/cities/<string:city_id>/places', methods=['GET'],
@@ -37,13 +38,13 @@ def post_Place(city_id):
     if cities is None:
         abort(404)
 
-    kwargs = request.get_json()
-    if not kwargs:
+    if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
 
-    if "user_id" not in kwargs:
+    if "user_id" not in request.get_json():
         return make_response(jsonify({'error': 'Missing user_id'}), 400)
 
+    kwargs = request.get_json()
     user = storage.get("User", kwargs['user_id'])
     if not user:
         abort(404)
@@ -55,8 +56,7 @@ def post_Place(city_id):
     kwargs['city_id'] = cities.id
 
     place = Place(**kwargs)
-    storage.new(place)
-    storage.save()
+    place.save()
     return make_response(jsonify(place.to_dict()), 201)
 
 
@@ -70,7 +70,8 @@ def put_Place(place_id):
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     for attr, val in request.get_json().items():
-        if attr not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
+        if attr not in ['id', 'user_id', 'city_id',
+                        'created_at', 'updated_at']:
             setattr(place, attr, val)
     place.save()
     return jsonify(place.to_dict())
